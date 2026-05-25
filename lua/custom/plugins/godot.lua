19737c13b8fdd6bb68131b285064bf266f06dba4
Alex Fargo <me@alexfargo.xyz>
2026-05-25 14:28:18 -0700

    godotdev

---
+++ lua/custom/plugins/godot.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'Mathijs-Bakker/godotdev.nvim',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {},
+  },
+}
