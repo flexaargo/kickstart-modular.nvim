162a5ad464c5f1bfbf02326a9555b372e7fc70aa
Alex Fargo <me@alexfargo.xyz>
2026-05-25 14:40:02 -0700

    godot formatter config, godot file configs

--- lua/custom/plugins/godot.lua
+++ lua/custom/plugins/godot.lua
@@ -7,6 +7,22 @@ return {
       'nvim-neotest/nvim-nio',
       'nvim-treesitter/nvim-treesitter',
     },
-    opts = {},
+    opts = {
+      formatter_cmd = { 'gdscript-formatter', '--use-spaces', '--indent-size', '2', '--reorder-code' },
+    },
+    config = function(_, opts)
+      require('godotdev').setup(opts)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('custom-godot-indent', { clear = true }),
+        pattern = 'gdscript',
+        callback = function()
+          vim.bo.expandtab = true
+          vim.bo.shiftwidth = 2
+          vim.bo.softtabstop = 2
+          vim.bo.tabstop = 2
+        end,
+      })
+    end,
   },
 }
